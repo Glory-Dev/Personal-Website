145cf19ab072e109de9b43d784ed12f14572bf3e
Glory <75427880+Glory-Dev@users.noreply.github.com>
2022-01-13 17:26:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,65 +1,109 @@
 const header = document.querySelector("body > .header");
+const navLinks = document.querySelector(".header .navbar > .links");
+const navLinksLis = document.querySelectorAll(".header .navbar > .links li");
+const menuBarIcon = document.querySelector(".header .navbar > .menu-icon");
 
 window.addEventListener("scroll", () => {
-  if (scrollY >= 50) {
-    header.classList.add("active");
+  scrollY >= 50
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+function resizeWindowForHeader() {
+  if (window.innerWidth > 575) {
+    navLinks.classList.remove("active");
+    menuBarIcon.classList.remove("active");
+  }
+}
+
+window.addEventListener("resize", resizeWindowForHeader());
+resizeWindowForHeader();
+
+menuBarIcon.addEventListener("click", toggleMenuBar);
+navLinksLis.forEach((li) => {
+  li.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menuBarIcon.classList.remove("active");
+  });
+});
+
+document.addEventListener("keyup", (e) => {
+  if (menuBarIcon.classList.contains("active"))
+    if (e.key == "Escape") toggleMenuBar();
+});
+
+function toggleMenuBar() {
+  menuBarIcon.classList.contains("active")
+    ? menuBarIcon.classList.remove("active")
+    : menuBarIcon.classList.add("active");
+  navLinks.classList.contains("active")
+    ? navLinks.classList.remove("active")
+    : navLinks.classList.add("active");
+}
+
+//
+
+const toolboxes = Array.from(
+  document.querySelectorAll(".skills-tools .toolbox > .card")
+);
+
+window.addEventListener("resize", function () {
+  console.log(window.innerWidth);
+
+  if (window.innerWidth > 900) {
+    toolboxes.forEach((card) => {
+      card.addEventListener("click", removeActive);
+    });
   } else {
-    header.classList.remove("active");
+    card.classList.remove("active");
   }
 });
 
-const toggleMenuBar = document.querySelector(".header .navbar > .menu-icon");
-const navLinks = document.querySelector(".header .navbar > .links");
-
-toggleMenuBar.onclick = () => {
-  toggleMenuBar.classList.toggle("active");
-  navLinks.classList.toggle("active");
-};
-
-//
-
-const switcherLis = document.querySelectorAll(".skills-tools .switcher li");
-const toolboxes = Array.from(
-  document.querySelectorAll(".skills-tools .toolbox > div")
-);
-
-switcherLis.forEach((li) => {
-  li.addEventListener("click", removeActive);
-  li.addEventListener("click", filterGallery);
-});
-
 function removeActive() {
-  switcherLis.forEach((li) => {
-    li.classList.remove("active");
+  toolboxes.forEach((card) => {
+    card.classList.remove("active");
     this.classList.add("active");
   });
 }
-function filterGallery() {
-  toolboxes.forEach((e) => {
-    e.style.visibility = "hidden";
-    document.querySelectorAll(this.dataset.switch).forEach((e) => {
-      e.style.visibility = "visible";
-    });
-  });
-}
 
 //
 
-// let html = document.querySelector("html");
-// let changeTheme = document.querySelector("body > .theme");
+// const switcherLis = document.querySelectorAll(".projects .switcher li");
+// const toolboxes = Array.from(
+//   document.querySelectorAll(".projects .content .gallery > div")
+// );
 
-// changeTheme.addEventListener("click", () => {});
+// switcherLis.forEach((li) => {
+//   li.addEventListener("click", removeActive);
+//   li.addEventListener("click", filterGallery);
+// });
+
+// function removeActive() {
+//   switcherLis.forEach((li) => {
+//     li.classList.remove("active");
+//     this.classList.add("active");
+//   });
+// }
+
+// function filterGallery() {
+//   toolboxes.forEach((e) => {
+//     e.style.visibility = "hidden";
+//     document.querySelectorAll(this.dataset.switch).forEach((e) => {
+//       e.style.visibility = "visible";
+//     });
+//   });
+// }
+
+//
 
 //
 
 const scrollToTop = document.querySelector("body > .scroll-top");
 
 window.addEventListener("scroll", () => {
-  if (scrollY >= 1300) {
-    scrollToTop.classList.add("show");
-  } else {
-    scrollToTop.classList.remove("show");
-  }
+  scrollY >= 1300
+    ? scrollToTop.classList.add("show")
+    : scrollToTop.classList.remove("show");
 });
 scrollToTop.addEventListener("click", () => {
   window.scrollTo({
--- style-guide.md
+++ style-guide.md
@@ -1,5 +1,32 @@
-# Fonts
+# Front-End Style Guide :)
 
-- `font-family: "Poppins", sans-serif;`
-- `font-family: "Cairo", sans-serif;`
-- `font-family: "Licorice", cursive;`
+## Typography
+
+### Body Copy
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins?query=Poppins)
+- Weights: 100, 200, 300, 400, 500, 600, 700, 800, 900
+- Styles: Normal, Italic
+
+#### Arabic
+
+- Family: [Cairo](https://fonts.google.com/specimen/Cairo)
+- Weights: 200, 300, 400, 500, 600, 700, 800, 900
+- Styles: Normal, Italic
+
+### Headings
+
+- Family: [Gugi](https://fonts.google.com/specimen/Gugi?query=Gug)
+- Weights: 400
+
+#### Landing Title
+
+- Family: [Major Mono Display](https://fonts.google.com/specimen/Major+Mono+Display?query=Major+Mono+Display)
+- Weights: 400
+
+## Icons
+
+We provide SVGs for the social icons. But please feel free to use a font icon library if you like. Some suggestions can be found below:
+
+- [Font Awesome](https://fontawesome.com)
+- [Flaticon (Webiste)](https://www.flaticon.com/)
